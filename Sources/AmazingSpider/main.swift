37c5b4409fc9ad65a01c46b92af7a6808032ec1e
yuezaixz <303178394@qq.com>
2017-08-16 23:13:29 +0800

    init base architecture

--- Sources/AmazingSpider/main.swift
+++ Sources/AmazingSpider/main.swift
@@ -1,1 +1,46 @@
-print("Hello, world!")
+import Foundation
+import CommandLineKit
+import Rainbow
+import AmazingSpiderKit
+import PathKit
+
+let cli = CommandLineKit.CommandLine()
+
+let help = BoolOption(shortFlag: "h", longFlag: "help",
+                      helpMessage: "Prints a help message.")
+
+let excludePathsOption = MultiStringOption(shortFlag: "e", longFlag: "exclude", helpMessage: "Excluded paths which should run spider.")
+
+cli.addOptions(excludePathsOption, help)
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.lightBlue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s:str, type: type)
+}
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+if help.value {
+    cli.printUsage()
+    exit(EX_OK)
+}
+
+let excludePaths = excludePathsOption.value ?? []
+
+print(excludePaths)
---
+++ Sources/AmazingSpiderKit/AmazingSpiderKit.swift
@@ -0,0 +1,8 @@
+//
+//  AmazingSpiderKit.swift
+//  AmazingSpiderPackageDescription
+//
+//  Created by 吴迪玮 on 2017/8/16.
+//
+
+import Foundation
